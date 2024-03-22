cf5e1d720cb44cac699f87dfef2d0bc4d6437629
Aiturganm <aiturganmaksatbekkyzy04@gmail.com>
2024-03-22 17:57:10 +0600

    Commit

--- src/test/java/project/service/serviceImpl/AnnouncementServiceImpl.java
+++ src/test/java/project/service/serviceImpl/AnnouncementServiceImpl.java
@@ -3,9 +3,9 @@ package project.service.serviceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import project.service.AnnouncementService;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class AnnouncementServiceImpl implements AnnouncementService {
+public class AnnouncementServiceImpl {
 }
