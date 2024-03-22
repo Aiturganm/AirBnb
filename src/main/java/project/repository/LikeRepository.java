d8f87722752b3607a51dda726b8837b0edd17752
macbook <ajybeksadykov@gmail.com>
2024-03-22 18:00:37 +0600

    house2 methods2

--- src/main/java/project/repository/LikeRepository.java
+++ src/main/java/project/repository/LikeRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import project.entities.Address;
 import project.entities.Like;
 
-public interface LikeRepository extends JpaRepository<Like, Long> {
+public interface Repository extends JpaRepository<Like, Long> {
 }
