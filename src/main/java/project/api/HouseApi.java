1815bf828b39ca3d23f8323abfd7b64d0a5323cc
macbook <ajybeksadykov@gmail.com>
2024-03-23 23:02:32 +0600

    house all methods22222

--- src/main/java/project/api/HouseApi.java
+++ src/main/java/project/api/HouseApi.java
@@ -1,6 +1,8 @@
 package project.api;
+import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import project.dto.request.HouseRequest;
 import project.dto.response.HouseResponse;
@@ -19,47 +21,62 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HouseApi {
     private final HouseService houseService;
-    @Secured("ADMIN")
+  @PermitAll
     @PostMapping("/saveHouse")
-    public SimpleResponse saveHouse(@RequestBody HouseRequest houseRequest) {
-        return houseService.saveHouse(houseRequest);
+    public SimpleResponse saveHouse(@RequestBody HouseRequest houseRequest, Principal principal ) {
+        return houseService.saveHouse(houseRequest , principal);
     }
+    @PermitAll
     @GetMapping("/getHouseById/{houseId}")
     public HouseResponse findById(@PathVariable Long houseId){
         return houseService.findbyId(houseId);
     }
-    @GetMapping("/findAllHouses")
+    @PermitAll
+    @GetMapping("/findAllHousesPublishedHouses")
     public List<HouseResponse> allHouses(){
         return houseService.findAll();
     }
+    @Secured("ADMIN")
+    @GetMapping("/findAllHousesNotPublishedHouses")
+    public List<HouseResponse> NotAllHouses(){
+        return houseService.notPublishedHouses();
+    }
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDER')")
     @PutMapping("/updateHouse/{houseId}")
     public SimpleResponse updateHouse(@RequestBody HouseRequest houseRequest, @PathVariable Long houseId, Principal principal){
         return houseService.updateHouse(houseRequest, houseId, principal);
     }
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDER')")
     @GetMapping("/deleteHouse/{houseId}")
     public SimpleResponse deleteHouse( @PathVariable Long houseId, Principal principal){
         return houseService.deleteHouse(houseId, principal);
     }
+    @PermitAll
     @GetMapping("/getHouseByName/{houseName}")
     public HouseResponse findByName(@RequestParam String houseName){
         return houseService.findByName(houseName);
     }
+    @PermitAll
     @GetMapping("/getHouseByUserId/{userId}")
     public List<UserHouseResponse> findByUserId(@PathVariable Long userId){
         return houseService.findByUserId(userId);
     }
+    @PermitAll
     @GetMapping("/sortByPrice/{ascOrDesc}")
     public List<HouseResponse> sortByPrice(@RequestParam String ascOrDesc){
         return houseService.sortByPrice(ascOrDesc);
     }
+    @PermitAll
     @GetMapping("/sortByBetweenPrice/{startPrice}/{FinishPrice}")
     public List<HouseResponse> betweenPrice(@PathVariable BigDecimal startPrice, @PathVariable BigDecimal FinishPrice){
         return houseService.betweenPrice(startPrice, FinishPrice);
     }
+    @PermitAll
     @GetMapping("/getHouseByRegion/{region}")
     public List<HouseResponse> findByRegion(@RequestParam Region region){
         return houseService.findByRegion(region);
     }
+    @PermitAll
     @GetMapping("/filterByHomeType/{type}")
     public List<HouseResponse> filteByType(@RequestParam HouseType type){
         return houseService.filterByType(type);
