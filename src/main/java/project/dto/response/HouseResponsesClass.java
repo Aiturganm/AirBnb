e30a795ad33ce92982e440ca60867e550b25ce44
nurkamil <nurkamilkamchiev@gmail.com>
2024-03-24 18:26:32 +0600

    user service about token jt

--- src/main/java/project/dto/response/HouseResponsesClass.java
+++ src/main/java/project/dto/response/HouseResponsesClass.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
 public class HouseResponsesClass {
     private Long id;
     private String nameOfHotel;
@@ -20,4 +19,15 @@ public class HouseResponsesClass {
     private HouseType houseType;
     private BigDecimal price;
     private int guests;
+
+    public HouseResponsesClass(Long id, String nameOfHotel, String description, List<String> images, byte room, HouseType houseType, BigDecimal price, int guests) {
+        this.id = id;
+        this.nameOfHotel = nameOfHotel;
+        this.description = description;
+        this.images = images;
+        this.room = room;
+        this.houseType = houseType;
+        this.price = price;
+        this.guests = guests;
+    }
 }
--- src/main/java/project/repository/AddressRepository.java
+++ src/main/java/project/repository/AddressRepository.java
@@ -1,7 +1,15 @@
 package project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.dto.response.HouseResponsesClass;
 import project.entities.Address;
+import project.enums.Region;
+
+import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    List<HouseResponsesClass> findByRegion(Region region);
+
+    boolean existsByStreet(String street);
 }
