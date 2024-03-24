71dc8996071ff4a0cf25c70c6b1a26089cc136d8
nurkamil <nurkamilkamchiev@gmail.com>
2024-03-24 19:00:49 +0600

    w

--- src/main/java/project/repository/AddressRepository.java
+++ src/main/java/project/repository/AddressRepository.java
@@ -2,6 +2,7 @@ package project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.dto.response.HouseResponsesClass;
 import project.entities.Address;
 import project.enums.HouseType;
@@ -11,9 +12,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
-   @Query("select new project.dto.response.HouseResponsesClass(a.house.id,a.house.nameOfHotel,a.house.description,a.house.images,a.house.room,a.house.houseType,a.house.price,a.house.guests)" +
-           " from Address a where a.region = :region")
-    List<HouseResponsesClass> findByRegion(Region region);
+    @Query("select new project.dto.response.HouseResponsesClass(a.house.id,a.house.nameOfHotel,a.house.description," +
+            "a.house.images,a.house.room,a.house.houseType,a.house.price,a.house.guests)" +
+            " from Address a where a.region = :region")
+    List<HouseResponsesClass> findByRegion(@Param("region") Region region);
 
     boolean existsByStreet(String street);
 }
