e9ebd8e0eccea5f45f65e22fbc1ee414dcb4d35a
Aiturganm <aiturganmaksatbekkyzy04@gmail.com>
2024-03-24 18:23:25 +0600

    Rent info

--- src/main/java/project/api/HouseApi.java
+++ src/main/java/project/api/HouseApi.java
@@ -1,4 +1,5 @@
 package project.api;
+
 import jakarta.annotation.security.PermitAll;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.annotation.Secured;
@@ -21,65 +22,77 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HouseApi {
     private final HouseService houseService;
-  @PermitAll
+
+    @PermitAll
     @PostMapping("/saveHouse")
-    public SimpleResponse saveHouse(@RequestBody HouseRequest houseRequest, Principal principal ) {
-        return houseService.saveHouse(houseRequest , principal);
+    public SimpleResponse saveHouse(@RequestBody HouseRequest houseRequest, Principal principal) {
+        return houseService.saveHouse(houseRequest, principal);
     }
+
     @PermitAll
     @GetMapping("/getHouseById/{houseId}")
-    public HouseResponse findById(@PathVariable Long houseId){
+    public HouseResponse findById(@PathVariable Long houseId) {
         return houseService.findbyId(houseId);
     }
+
     @PermitAll
     @GetMapping("/findAllHousesPublishedHouses")
-    public List<HouseResponse> allHouses(){
+    public List<HouseResponse> allHouses() {
         return houseService.findAll();
 
     }
+
     @Secured("ADMIN")
     @GetMapping("/findAllHousesNotPublishedHouses")
-    public List<HouseResponse> NotAllHouses(){
+    public List<HouseResponse> NotAllHouses() {
         return houseService.notPublishedHouses();
     }
+
     @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDER')")
     @PutMapping("/updateHouse/{houseId}")
-    public SimpleResponse updateHouse(@RequestBody HouseRequest houseRequest, @PathVariable Long houseId, Principal principal){
+    public SimpleResponse updateHouse(@RequestBody HouseRequest houseRequest, @PathVariable Long houseId, Principal principal) {
         return houseService.updateHouse(houseRequest, houseId, principal);
     }
+
     @PreAuthorize("hasAnyAuthority('ADMIN', 'VENDER')")
     @GetMapping("/deleteHouse/{houseId}")
-    public SimpleResponse deleteHouse( @PathVariable Long houseId, Principal principal){
+    public SimpleResponse deleteHouse(@PathVariable Long houseId, Principal principal) {
         return houseService.deleteHouse(houseId, principal);
     }
+
     @PermitAll
     @GetMapping("/getHouseByName")
-    public HouseResponse findByName(@RequestParam String houseName){
+    public HouseResponse findByName(@RequestParam String houseName) {
         return houseService.findByName(houseName);
     }
+
     @PermitAll
     @GetMapping("/getHouseByUserId/{userId}")
-    public List<UserHouseResponse> findByUserId(@PathVariable Long userId){
+    public List<UserHouseResponse> findByUserId(@PathVariable Long userId) {
         return houseService.findByUserId(userId);
     }
+
     @PermitAll
     @GetMapping("/sortByPrice")
-    public List<HouseResponse> sortByPrice(@RequestParam String ascOrDesc){
+    public List<HouseResponse> sortByPrice(@RequestParam String ascOrDesc) {
         return houseService.sortByPrice(ascOrDesc);
     }
+
     @PermitAll
     @GetMapping("/sortByBetweenPrice/{startPrice}/{FinishPrice}")
-    public List<HouseResponse> betweenPrice(@PathVariable BigDecimal startPrice, @PathVariable BigDecimal FinishPrice){
+    public List<HouseResponse> betweenPrice(@PathVariable BigDecimal startPrice, @PathVariable BigDecimal FinishPrice) {
         return houseService.betweenPrice(startPrice, FinishPrice);
     }
+
     @PermitAll
     @GetMapping("/getHouseByRegion")
-    public List<HouseResponse> findByRegion(@RequestParam Region region){
+    public List<HouseResponse> findByRegion(@RequestParam Region region) {
         return houseService.findByRegion(region);
     }
+
     @PermitAll
     @GetMapping("/filterByHomeType")
-    public List<HouseResponse> filterByType(@RequestParam HouseType type){
+    public List<HouseResponse> filterByType(@RequestParam HouseType type) {
         return houseService.filterByType(type);
     }
 
